4e1cf433ac386ab09d7bb77ab1185081430b659c
kosbys <kosmicabysslord@protonmail.com>
2022-06-16 20:36:04 +0300

    Add menu card class

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,13 @@
+class MenuCard {
+    constructor(name, price, salePrice, description, image) {
+        this.name = name;
+        this.price = price;
+        this.salePrice = salePrice;
+        this.description = description;
+        this.image = image;
+    }
+}
+
 function menu() {
     const container = document.createElement('div');
     container.classList.add('container');
